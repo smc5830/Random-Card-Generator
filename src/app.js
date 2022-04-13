4adfa914b40c93fb154d30aff0cc4517b953db13
smc5830 <s.molinacarbajal@gmail.com>
2022-04-13 12:00:58 +0000

    Coplete Exercise

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  /*   document.querySelector(".icon").classList.add(generateRandomIcon1());
+  document.querySelector(".icon").classList.add(generateRandomIcon1()); */
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  generateRandomIcon1();
+};
+let AllIcons = document.querySelectorAll(".icon");
+
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let nNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[nNumbers];
+}
+let icon = ["♦", "♠", "♥", "♣"];
+const generateRandomIcon1 = () => {
+  let nIcon = icon[Math.floor(Math.random() * icon.length)];
+  for (let icons = 0; icons < AllIcons.length; icons++) {
+    AllIcons[icons].innerHTML = nIcon;
+    if (nIcon == "♦" || nIcon == "♥") {
+      AllIcons[icons].style.color = "red";
+    }
+  }
 };
